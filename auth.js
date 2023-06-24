973a02eb7e23d8678401dded817cd3f19884ec8d
Kfrancis2018 <65624748+Kfrancis2018@users.noreply.github.com>
2023-06-24 14:12:59 -0400

    Basic Database setup

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 
 
+
 function login(){
     email=document.getElementById("email").value;
     password=document.getElementById("password").value;
@@ -30,3 +31,49 @@ function signout(){
         console.error('Sign Out Error', error);
       });
 }
+
+function set(){
+
+    var fname=document.getElementById("fname").value;
+    var lname=document.getElementById("lname").value;
+    var organization=document.getElementById("organization").value;
+    var organization=document.getElementById("birthday").value;
+    var title=document.getElementById("title").value;
+    var UserID= auth.currentUser.uid;
+
+    db.collection(UserID).doc("Vcard").set({
+        fname :fname, 
+        lname:lname,
+        organization:organization,
+        birthday:birthday,
+        title:title, 
+
+    }).then(() =>{
+            "Vcard Created"
+    }
+    
+    
+    ).catch((error)=>{
+        "Error Writing Document", error
+    });
+}
+function show(){
+    var UserID = auth.currentUser.uid;
+    db.collection(UserID).doc("Vcard").get().then((doc)=> {
+        if(doc.exists){
+            console.log("document data" , doc.data() );
+            document.getElementById("fnameshow").innerHTML=doc.data().fname;
+            document.getElementById("lnameshow").innerHTML=doc.data().lname;
+            document.getElementById("organizationshow").innerHTML=doc.data().organization;
+            document.getElementById("birthdayshow").innerHTML=doc.data().birthday;
+            document.getElementById("titleshow").innerHTML=doc.data().title;
+        }else{
+            console.log("no such document");
+        }
+
+
+    })
+    
+
+    }
+
--- config.js
+++ config.js
@@ -1,16 +1,16 @@
+
 const firebaseConfig = {
-    apiKey: "AIzaSyA0mfxslt6iV1CNCpx8syBMdYOgRWkZ1PU",
-    authDomain: "finalproject-bizzqr.firebaseapp.com",
-    projectId: "finalproject-bizzqr",
-    storageBucket: "finalproject-bizzqr.appspot.com",
-    messagingSenderId: "1002524993571",
-    appId: "1:1002524993571:web:4be2a34495a4780e182996",
-    measurementId: "G-5TLLKD833Z"
-  };
+  apiKey: "AIzaSyA0mfxslt6iV1CNCpx8syBMdYOgRWkZ1PU",
+  authDomain: "finalproject-bizzqr.firebaseapp.com",
+  projectId: "finalproject-bizzqr",
+  storageBucket: "finalproject-bizzqr.appspot.com",
+  messagingSenderId: "1002524993571",
+  appId: "1:1002524993571:web:4be2a34495a4780e182996",
+  measurementId: "G-5TLLKD833Z"
+};
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-  const auth =firebase.auth();
-  const db =firebase.firestore();
+const auth =firebase.auth();
 
-  
+const db =firebase.firestore();
